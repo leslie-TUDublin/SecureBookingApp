b8a003f4b0e49a7524c962ba826cbfd330db06a7
Leslie Kaspruk <C24743569@mytudublin.ie>
2026-07-29 17:59:46 +0300

    START Spring Boot project

---
+++ src/test/java/com/lesya/booking1/Booking1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesya.booking1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Booking1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
